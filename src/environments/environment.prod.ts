081986d8667ddb668ad3d710225d19ece40f1d19
hechtag <jm.hecht@gmx.de>
2020-03-28 23:16:59 +0100

    fix bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAFEGIBasSRQXeT5nUifKMFuiMoMO3AAe0',
+    authDomain: 'ng-startseite-f1a7f.firebaseapp.com',
+    databaseURL: 'https://ng-startseite-f1a7f.firebaseio.com',
+    projectId: 'ng-startseite-f1a7f',
+    storageBucket: 'ng-startseite-f1a7f.appspot.com',
+    messagingSenderId: '671840974442',
+    appId: '1:671840974442:web:188949467141ddeafabc13'
+  }
 };
