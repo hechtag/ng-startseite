5123c6308d96e0f601172626f790c16879fe4bb3
hechtag <jm.hecht@gmx.de>
2020-04-02 22:45:15 +0200

    mems and cards per user

--- src/app/core/fire-store.service.ts
+++ src/app/core/fire-store.service.ts
@@ -2,42 +2,57 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Memory } from '../memory-list/shared/memory';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, switchMap } from 'rxjs/operators';
 import { Entity } from './entity.model';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FireStoreService {
 
-  constructor(private db: AngularFirestore) { }
+  userPath$: Observable<string>;
+  constructor(
+    private db: AngularFirestore,
+    private auth: AuthService,
+  ) {
+    this.userPath$ = this.auth.user$.pipe(map(user => `users/${user.uid}/`));
+  }
 
   getCollectionData$<T>(collectionName: string): Observable<Entity<T>[]> {
-    return this.db.collection(collectionName).snapshotChanges().pipe(map(s => s.map(d => {
-      const data = d.payload.doc.data() as T;
-      return { data, id: d.payload.doc.id } as Entity<T>;
-    })));
-
+    return this.userPath$.pipe(
+      switchMap(path => this.db.collection(path + collectionName).snapshotChanges()),
+      map(s => s.map(d => {
+        const data = d.payload.doc.data() as T;
+        return { data, id: d.payload.doc.id } as Entity<T>;
+      }))
+    );
   }
+
   createEntity<T>(collectionName: string, record: T) {
-    this.db.collection(collectionName).add(record);
+    this.userPath$.subscribe(path => {
+      this.db.collection(path + collectionName).add(record);
+    });
   }
 
   deleteEntity(collectionName: string, id: string) {
-    this.db.collection(collectionName).doc(id).delete();
+    this.userPath$.subscribe(path => {
+      this.db.collection(path + collectionName).doc(id).delete();
+    });
   }
 
   getDocument<T>(collectionName, id: string): Observable<Entity<T>> {
-    return this.db.collection(collectionName)
-      .doc(id)
-      .valueChanges()
-      .pipe(map(d => {
+    return this.userPath$.pipe(
+      switchMap(path => this.db.collection(path + collectionName)
+        .doc(id)
+        .valueChanges()),
+      map(d => {
         return { id, data: d } as Entity<T>;
       }));
   }
 
   patchEntity<T>(collectionName: string, id: string, patchRecord: T) {
-    this.db.collection(collectionName).doc(id).get().subscribe(doc => {
+    this.userPath$.pipe(switchMap(path => this.db.collection(path + collectionName).doc(id).get())).subscribe(doc => {
       const patchedDoc = { ...doc.data(), ...patchRecord };
       this.db.collection(collectionName).doc(id).set(patchedDoc);
     });
